6f8966aee5bd86bc9516e4d036d3ba3532d3a849
rc21292 <rc21292@gmail.com>
2023-01-14 17:01:13 +0530

    uploading all files

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -7,6 +7,7 @@ use App\Models\Admin;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -50,7 +51,47 @@ class AdminController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'firstname'                => 'required',
+            'employee_code'            => 'required',
+            'designation'              => 'required',           
+            'email'                    => 'required|unique:admins',           
+            'phone'                    => 'required',
+            'department'               => 'required',
+            'kra'                      => 'required',
+            'linked_employee'          => 'required',
+            'linked_employee_id'       => 'required',
+        ];
+
+        $messages = [
+            'firstname.required'             => 'Please enter firstname',
+            'employee_code.required'         => 'Please enter employee code.',
+            'designation.required'           => 'Please enter designation.',          
+            'email.required'                 => 'Please enter official mail ID.',         
+            'phone.required'                 => 'Please enter contact number.',
+            'department.required'            => 'Please select department.',
+            'kra.required'                   => 'Please enter KRA',           
+            'linked_employee.required'       => 'Please select linked employee.',
+            'linked_employee_id.required'    => 'Please enter linked employee ID.',
+        ];
+
+        $this->validate($request, $rules, $messages);
+
+        $admin                    =   Admin::create([
+            'firstname'           =>  $request->firstname,
+            'lastname'            =>  $request->lastname,
+            'employee_code'       =>  $request->employee_code,
+            'designation'         =>  $request->designation,           
+            'email'               =>  $request->email,
+            'phone'               =>  $request->phone,
+            'password'            =>  Hash::make('12345678'),
+            'department'          =>  $request->department,
+            'kra'                 =>  $request->kra,
+            'linked_employee'     =>  $request->linked_employee,
+            'linked_employee_id'  =>  $request->linked_employee_id
+        ]);
+
+        return redirect()->route('admin.admins.index')->with('success', 'Admin created successfully');
     }
 
     /**
@@ -87,7 +128,46 @@ class AdminController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $rules = [
+            'firstname'                => 'required',
+            'employee_code'            => 'required',
+            'designation'              => 'required',           
+            'email'                    => 'required|unique:admins,email,'.$id,           
+            'phone'                    => 'required',
+            'department'               => 'required',
+            'kra'                      => 'required',
+            'linked_employee'          => 'required',
+            'linked_employee_id'       => 'required',
+        ];
+
+        $messages = [
+            'firstname.required'             => 'Please enter firstname',
+            'employee_code.required'         => 'Please enter employee code.',
+            'designation.required'           => 'Please enter designation.',          
+            'email.required'                 => 'Please enter official mail ID.',         
+            'phone.required'                 => 'Please enter contact number.',
+            'department.required'            => 'Please select department.',
+            'kra.required'                   => 'Please enter KRA',           
+            'linked_employee.required'       => 'Please select linked employee.',
+            'linked_employee_id.required'    => 'Please enter linked employee ID.',
+        ];
+
+        $this->validate($request, $rules, $messages);
+
+        $admin                    =   Admin::where('id', $id)->update([
+            'firstname'           =>  $request->firstname,
+            'lastname'            =>  $request->lastname,
+            'employee_code'       =>  $request->employee_code,
+            'designation'         =>  $request->designation,           
+            'email'               =>  $request->email,
+            'phone'               =>  $request->phone,
+            'department'          =>  $request->department,
+            'kra'                 =>  $request->kra,
+            'linked_employee'     =>  $request->linked_employee,
+            'linked_employee_id'  =>  $request->linked_employee_id
+        ]);
+
+        return redirect()->route('admin.admins.index')->with('success', 'Admin updated successfully');
     }
 
     /**
--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -7,6 +7,7 @@ use App\Models\Admin;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -50,7 +51,47 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'firstname'                => 'required',
+            'employee_code'            => 'required',
+            'designation'              => 'required',           
+            'email'                    => 'required|unique:users',           
+            'phone'                    => 'required',
+            'department'               => 'required',
+            'kra'                      => 'required',
+            'linked_employee'          => 'required',
+            'linked_employee_id'       => 'required',
+        ];
+
+        $messages = [
+            'firstname.required'             => 'Please enter firstname',
+            'employee_code.required'         => 'Please enter employee code.',
+            'designation.required'           => 'Please enter designation.',          
+            'email.required'                 => 'Please enter official mail ID.',         
+            'phone.required'                 => 'Please enter contact number.',
+            'department.required'            => 'Please select department.',
+            'kra.required'                   => 'Please enter KRA',           
+            'linked_employee.required'       => 'Please select linked employee.',
+            'linked_employee_id.required'    => 'Please enter linked employee ID.',
+        ];
+
+        $this->validate($request, $rules, $messages);
+
+        $admin                    =   User::create([
+            'firstname'           =>  $request->firstname,
+            'lastname'            =>  $request->lastname,
+            'employee_code'       =>  $request->employee_code,
+            'designation'         =>  $request->designation,           
+            'email'               =>  $request->email,
+            'phone'               =>  $request->phone,
+            'password'            =>  Hash::make('12345678'),
+            'department'          =>  $request->department,
+            'kra'                 =>  $request->kra,
+            'linked_employee'     =>  $request->linked_employee,
+            'linked_employee_id'  =>  $request->linked_employee_id
+        ]);
+
+        return redirect()->route('admin.users.index')->with('success', 'User created successfully');
     }
 
     /**
@@ -87,7 +128,46 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $rules = [
+            'firstname'                => 'required',
+            'employee_code'            => 'required',
+            'designation'              => 'required',           
+            'email'                    => 'required|unique:users,email,'.$id,           
+            'phone'                    => 'required',
+            'department'               => 'required',
+            'kra'                      => 'required',
+            'linked_employee'          => 'required',
+            'linked_employee_id'       => 'required',
+        ];
+
+        $messages = [
+            'firstname.required'             => 'Please enter firstname',
+            'employee_code.required'         => 'Please enter employee code.',
+            'designation.required'           => 'Please enter designation.',          
+            'email.required'                 => 'Please enter official mail ID.',         
+            'phone.required'                 => 'Please enter contact number.',
+            'department.required'            => 'Please select department.',
+            'kra.required'                   => 'Please enter KRA',           
+            'linked_employee.required'       => 'Please select linked employee.',
+            'linked_employee_id.required'    => 'Please enter linked employee ID.',
+        ];
+
+        $this->validate($request, $rules, $messages);
+
+        $user                    =   User::where('id', $id)->update([
+            'firstname'           =>  $request->firstname,
+            'lastname'            =>  $request->lastname,
+            'employee_code'       =>  $request->employee_code,
+            'designation'         =>  $request->designation,           
+            'email'               =>  $request->email,
+            'phone'               =>  $request->phone,
+            'department'          =>  $request->department,
+            'kra'                 =>  $request->kra,
+            'linked_employee'     =>  $request->linked_employee,
+            'linked_employee_id'  =>  $request->linked_employee_id
+        ]);
+
+        return redirect()->route('admin.users.index')->with('success', 'User updated successfully');
     }
 
     /**
