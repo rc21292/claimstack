1fa2ee01edc1010e715be7392b11eb1849c2ff9e
rc21292 <rc21292@gmail.com>
2023-01-14 17:07:25 +0530

    uploading all files

--- resources/views/associate/sections/flash-message.blade.php
+++ resources/views/associate/sections/flash-message.blade.php
@@ -1,27 +1,27 @@
 @if ($message = Session::get('success'))
-<div class="alert alert-success" role="alert">
-    <button type="button" class="close" data-dismiss="alert">×</button>
-    <strong>{{ $message }}</strong>
-</div>
+    <div class="alert alert-success alert-dismissible bg-success text-white border-0 fade show" role="alert">
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <strong>{{ $message }}</strong>
+    </div>
 @endif
 
 @if ($message = Session::get('error'))
-<div class="alert alert-danger" role="alert">
-    <button type="button" class="close" data-dismiss="alert">×</button>
-    <strong>{{ $message }}</strong>
-</div>
+    <div class="alert alert-danger alert-dismissible bg-danger text-white border-0 fade show" role="alert">
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <strong>{{ $message }}</strong>
+    </div>
 @endif
 
 @if ($message = Session::get('warning'))
-<div class="alert alert-warning bg-warning text-white border-0" role="alert">
-    <button type="button" class="close" data-dismiss="alert">×</button>
-    <strong>{{ $message }}</strong>
-</div>
+    <div class="alert alert-warning alert-dismissible bg-warning border-0 fade show" role="alert">
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <strong>{{ $message }}</strong>
+    </div>
 @endif
 
 @if ($message = Session::get('info'))
-<div class="alert alert-light bg-light text-dark border-0" role="alert">
-    <button type="button" class="close" data-dismiss="alert">×</button>
-    <strong>{{ $message }}</strong>
-</div>
+    <div class="alert alert-primary alert-dismissible bg-primary text-white border-0 fade show" role="alert">
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <strong>{{ $message }}</strong>
+    </div>
 @endif
