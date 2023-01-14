8bc968c66b21fad03ae1ded0a0814b1d25ade5ae
rc21292 <rc21292@gmail.com>
2023-01-14 17:42:35 +0530

    uploading all files

--- resources/views/layouts/employee.blade.php
+++ resources/views/layouts/employee.blade.php
@@ -19,7 +19,7 @@
 </head>
 
 <body class="loading"
-    data-layout-config='{"leftSideBarTheme":"dark","layoutBoxed":false, "leftSidebarCondensed":false, "leftSidebarScrollable":false,"darkMode":false, "showRightSidebarOnStart": true}'>
+    data-layout-config='{"leftSideBarTheme":"dark","layoutBoxed":false, "leftSidebarCondensed":false, "leftSidebarScrollable":false,"darkMode":false, "showRightSidebarOnStart": false}'>
     <!-- Begin page -->
     <div class="wrapper">
         @include('employee.sections.sidebar')
