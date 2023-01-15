ace91e662ede8f5e6f3e3735738b68c12ed9e1bc
Krishna Mishra <mkrishna@n2rtechnologies.in>
2023-01-15 21:07:37 +0530

    model with logs

--- app/Exports/ExportAdmin.php
+++ app/Exports/ExportAdmin.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Admin;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ExportAdmin implements FromCollection
+class ExportAdmin implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +14,25 @@ class ExportAdmin implements FromCollection
     public function collection()
     {
         return Admin::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Id',
+            'Firstname',
+            'Lastname',
+            'Email',
+            'Employee Code',
+            'Designation',
+            'Department',
+            'Phone',
+            'Password',
+            'Linked Employee',
+            'Linked Employee Id',
+            'Kra',
+            'Created At',
+            'Updated At'
+        ];
     }
 }
--- app/Exports/ExportAssociatePartner.php
+++ app/Exports/ExportAssociatePartner.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\AssociatePartner;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ExportAssociatePartner implements FromCollection
+class ExportAssociatePartner implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +14,45 @@ class ExportAssociatePartner implements FromCollection
     public function collection()
     {
         return AssociatePartner::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Id',
+            'firstname',
+            'lastname',
+            'associate_partner_id',
+            'type',
+            'pan',
+            'panfile',
+            'owner',
+            'email',
+            'address',
+            'city',
+            'state',
+            'pincode',
+            'password',
+            'phone',
+            'reference',
+            'status',
+            'linked_associate_partner',
+            'linked_associate_partner_id',
+            'assigned_employee',
+            'assigned_employee_id',
+            'linked_employee',
+            'linked_employee_id',
+            'mou',
+            'moufile',
+            'agreement_start_date',
+            'agreementfile',
+            'agreement_end_date',
+            'contact_person',
+            'contact_person_phone',
+            'contact_person_email',
+            'comments',
+            'Created At',
+            'Updated At'
+        ];
     }
 }
