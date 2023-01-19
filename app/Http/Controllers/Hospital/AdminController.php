d0b5c16dff315e0e560f9173693858bc17373bbf
rc21292 <rc21292@gmail.com>
2023-01-19 19:23:13 +0530

    uploading all files

--- app/Http/Controllers/Hospital/AdminController.php
+++ app/Http/Controllers/Hospital/AdminController.php
@@ -63,7 +63,7 @@ class AdminController extends Controller
         $rules = [
             'firstname'                => 'required|string|max:15',
             'lastname'                 => isset($request->lastname) ? 'string|max:30' : '',
-            'uid'                      => 'required|unique:admins|unique:users|alpha_num|max:8',
+            'uid'                      => 'required|unique:admins|unique:users|string|max:8',
             'designation'              => 'required|string|max:30',
             'email'                    => 'required|email|unique:admins|unique:users|unique:hospitals|unique:associate_partners|unique:employees',
             'phone'                    => 'required|numeric|digits:10',
@@ -154,7 +154,7 @@ class AdminController extends Controller
         $rules = [
             'firstname'                => 'required|string|max:15',
             'lastname'                 => isset($request->lastname) ? 'string|max:30' : '',
-            'uid'                      => 'required|alpha_num|max:8|unique:users|unique:admins,uid,'.$id,
+            'uid'                      => 'required|string|max:8|unique:users|unique:admins,uid,'.$id,
             'designation'              => 'required|string|max:30',
             'email'                    => 'required|email|unique:users|unique:hospitals|unique:associate_partners|unique:employees|unique:admins,email,'.$id,
             'phone'                    => 'required|numeric|digits:10',
