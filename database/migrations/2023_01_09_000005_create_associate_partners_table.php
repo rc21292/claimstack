06cf21a2d7a256927d41efb1fa589a16fda813d1
rc21292 <rc21292@gmail.com>
2023-01-20 10:24:54 +0530

    uploading all files

--- database/migrations/2023_01_09_000005_create_associate_partners_table.php
+++ database/migrations/2023_01_09_000005_create_associate_partners_table.php
@@ -57,7 +57,6 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
