ca1787487ddcf7ffc34b2919420452112231e190
Krishna Mishra <mkrishna@n2rtechnologies.in>
2023-03-11 13:51:07 +0530

    chnages

--- resources/views/super-admin/claims/claims/create/create.blade.php
+++ resources/views/super-admin/claims/claims/create/create.blade.php
@@ -142,10 +142,10 @@
         var category_option = $('#system_of_medicine').val();
         switch (category_option) {
                 case 'Allopathy':
-                    $("#treatment_category").val("");                   
+                    $("#treatment_category").val("{{ old('treatment_category', '') }}").removeAttr('disabled');                   
                     break;                
                 default:
-                    $("#treatment_category").val("Non Allopathic");                   
+                    $("#treatment_category").val("Non Allopathic").attr('disabled', true);                  
                     break;
             }
     }
