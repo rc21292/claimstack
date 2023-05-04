cf23a84164717aeb52050134187fbb67b0a13806
Krishna Mishra <mkrishna@n2rtechnologies.in>
2023-05-04 21:41:09 +0530

    chnages

--- resources/views/super-admin/assigning-status/final-assessment/manage.blade.php
+++ resources/views/super-admin/assigning-status/final-assessment/manage.blade.php
@@ -109,14 +109,26 @@ use Carbon\Carbon;
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 <script>
-$(function() {
-    
-  $('input[name="date_from_to"]').daterangepicker({
-    opens: 'left'
-  }, function(start, end, label) {
-    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-  });
-});
+
+    $(function() {
+
+        $('input[name="date_from_to"]').daterangepicker({
+            autoUpdateInput: false,
+            locale: {
+                cancelLabel: 'Clear'
+            }
+        });
+
+        $('input[name="date_from_to"]').on('apply.daterangepicker', function(ev, picker) {
+            $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+        });
+
+        $('input[name="date_from_to"]').on('cancel.daterangepicker', function(ev, picker) {
+            $(this).val('');
+        });
+
+    });
+
 </script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script type="text/javascript">
