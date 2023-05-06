4900737fa5259e3e92f49bc6ce3bf3224b972648
Krishna Mishra <mkrishna@n2rtechnologies.in>
2023-05-06 21:44:45 +0530

    chnages

--- resources/views/super-admin/hospitals/import-export.blade.php
+++ resources/views/super-admin/hospitals/import-export.blade.php
@@ -30,14 +30,24 @@
                     <div class="card-body">
                         <form action="{{ route('super-admin.hospitals.import') }}" method="POST" enctype="multipart/form-data">
                             @csrf
-                            <div class="form-group mb-4">
-                                <div class="custom-file text-left">
-                                    <input type="file" required name="file" class="custom-file-input" id="customFile">
-                                    <label class="custom-file-label" for="customFile">Choose file</label>
+
+                            <div class="form-group row mb-4">
+                                <div class="col-4">
+                                    <div class="input-group">
+                                        <input type="file" name="file" id="file_hospita_import" hidden="" onchange="$('label[for=' + $(this).attr('id') + ']').removeClass('btn-primary');$('label[for=' + $(this).attr('id') + ']').addClass('btn-warning');">
+                                        <label for="file_hospita_import" class="btn btn-primary upload-label"><i class="mdi mdi-upload"></i> Import Hospitals</label>
+                                    </div>
+                                </div>
+
+                                <div class="col-4">
+
+                                    <div class="input-group">
+                                        <a href="{{ route('super-admin.hospitals.export') }}" download="" class="btn btn-danger download-label"><i class="mdi mdi-download"></i> Export Hospitals </a>
+
+                                    </div>
                                 </div>
                             </div>
-                            <button class="btn btn-primary">Import Hospital</button>
-                            <a class="btn btn-success" href="{{ route('super-admin.hospitals.export') }}">Export Hospitals</a>
+                            
                         </form>
                     </div>
                 </div>
