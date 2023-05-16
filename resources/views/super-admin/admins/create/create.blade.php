09c95bd8964396bf7dda70b29e9cfa225ef4f79e
Krishna Mishra <er.krishna.mishra@gmail.com>
2023-05-16 06:07:04 -0700

    ddd

--- resources/views/super-admin/admins/create/create.blade.php
+++ resources/views/super-admin/admins/create/create.blade.php
@@ -61,50 +61,6 @@
                                     @enderror
                                 </div>
                                 <div class="col-md-6 mt-3">
-                                    <label for="designation">Designation <span class="text-danger">*</span></label>
-                                    <input type="text" onkeydown="return /[a-z, ]/i.test(event.key)"  class="form-control" id="designation" name="designation"
-                                        maxlength="30" placeholder="Enter designation" value="{{ old('designation') }}">
-                                    @error('designation')
-                                        <span id="name-error" class="error invalid-feedback">{{ $message }}</span>
-                                    @enderror
-                                </div>
-
-                                <div class="col-md-6 mt-3">
-                                    <label for="owner">Official Mail ID <span class="text-danger">*</span></label>
-                                    <input type="text" class="form-control" id="email" name="email" maxlength="30"
-                                        placeholder="Enter official mail ID" value="{{ old('email') }}">
-                                    @error('email')
-                                        <span id="name-error" class="error invalid-feedback">{{ $message }}</span>
-                                    @enderror
-                                </div>
-
-                                <div class="col-md-6 mt-3">
-                                    <label for="phone">Mobile Number <span class="text-danger">*</span></label>
-                                    <div class="input-group">
-                                        <label class="input-group-text" for="phone">+91</label>
-                                        <input type="number" class="form-control" id="phone" name="phone"
-                                            pattern="/^-?\d+\.?\d*$/" onKeyPress="if(this.value.length==10) return false;"
-                                            placeholder="Enter mobile number" value="{{ old('phone') }}">
-                                    </div>
-                                    @error('phone')
-                                        <span id="name-error" class="error invalid-feedback">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                                <div class="col-md-12 mt-3">
-                                    <label for="linked_with_superadmin">Linked With Super Admin <span class="text-danger">*</span></label>
-                                    <select class="form-select" id="linked_with_superadmin" name="linked_with_superadmin">
-                                        <option value="">Select</option>
-                                        <option value="yes"
-                                            {{ old('linked_with_superadmin') == 'yes' ? 'selected' : '' }}>Yes
-                                        </option>
-                                        <option value="no" {{ old('linked_with_superadmin') == 'no' ? 'selected' : '' }}>No
-                                        </option>
-                                    </select>
-                                    @error('linked_with_superadmin')
-                                        <span id="name-error" class="error invalid-feedback">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                                <div class="col-md-12 mt-3">
                                     <label for="department">Department <span class="text-danger">*</span></label>
                                     <select class="form-select" id="department" name="department">
                                         <option value="">Select Department</option>
@@ -144,6 +100,50 @@
                                         </option>
                                     </select>
                                     @error('department')
+                                        <span id="name-error" class="error invalid-feedback">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                                <div class="col-md-6 mt-3">
+                                    <label for="designation">Designation <span class="text-danger">*</span></label>
+                                    <input type="text" onkeydown="return /[a-z, ]/i.test(event.key)"  class="form-control" id="designation" name="designation"
+                                        maxlength="30" placeholder="Enter designation" value="{{ old('designation') }}">
+                                    @error('designation')
+                                        <span id="name-error" class="error invalid-feedback">{{ $message }}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-md-6 mt-3">
+                                    <label for="owner">Official Mail ID <span class="text-danger">*</span></label>
+                                    <input type="text" class="form-control" id="email" name="email" maxlength="30"
+                                        placeholder="Enter official mail ID" value="{{ old('email') }}">
+                                    @error('email')
+                                        <span id="name-error" class="error invalid-feedback">{{ $message }}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-md-6 mt-3">
+                                    <label for="phone">Mobile Number <span class="text-danger">*</span></label>
+                                    <div class="input-group">
+                                        <label class="input-group-text" for="phone">+91</label>
+                                        <input type="number" class="form-control" id="phone" name="phone"
+                                            pattern="/^-?\d+\.?\d*$/" onKeyPress="if(this.value.length==10) return false;"
+                                            placeholder="Enter mobile number" value="{{ old('phone') }}">
+                                    </div>
+                                    @error('phone')
+                                        <span id="name-error" class="error invalid-feedback">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                                <div class="col-md-6 mt-3">
+                                    <label for="linked_with_superadmin">Linked With Super Admin <span class="text-danger">*</span></label>
+                                    <select class="form-select" id="linked_with_superadmin" name="linked_with_superadmin">
+                                        <option value="">Select</option>
+                                        <option value="yes"
+                                            {{ old('linked_with_superadmin') == 'yes' ? 'selected' : '' }}>Yes
+                                        </option>
+                                        <option value="no" {{ old('linked_with_superadmin') == 'no' ? 'selected' : '' }}>No
+                                        </option>
+                                    </select>
+                                    @error('linked_with_superadmin')
                                         <span id="name-error" class="error invalid-feedback">{{ $message }}</span>
                                     @enderror
                                 </div>
