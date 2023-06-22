6181b078ee4b6d2b681a262d77efe10910447cbd
Krishna Mishra <er.krishna.mishra@gmail.com>
2023-06-22 23:31:29 +0530

    chnages

--- app/Exports/ExportHospital.php
+++ app/Exports/ExportHospital.php
@@ -6,15 +6,16 @@ use App\Models\Hospital;
 use App\Models\Admin;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class ExportHospital implements FromCollection, WithHeadings
+class ExportHospital implements FromCollection, WithHeadings, ShouldAutoSize
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        $admins = Hospital::latest('id')->select('id','uid','name','onboarding','by','address','city','state','pincode','firstname','lastname','pan','email','landline','phone','rohini','linked_associate_partner','linked_associate_partner_id','assigned_employee','assigned_employee_id','linked_employee','linked_employee_id','comments',DB::raw("DATE_FORMAT(hospitals.created_at, '%d-%m-%Y %H:%i:%s')"))->get();
+        $admins = Hospital::latest('id')->select('id','uid', 'name', 'onboarding', 'by', 'hospital_by', 'hms_hospital_id', 'address', 'city', 'state', 'pincode', 'firstname', 'lastname', 'pan', 'email',  'code', 'landline', 'phone', 'rohini', 'linked_employee_department', 'linked_associate_partner', 'linked_associate_partner_id', 'assigned_employee_department', 'assigned_employee', 'assigned_employee_id', 'linked_employee', 'linked_employee_id', 'tan', 'gst', 'owner_email', 'owner_phone', 'owner_pan', 'owner_aadhar', 'contact_person_firstname', 'contact_person_lastname', 'contact_person_email', 'contact_person_phone', 'registration_no', 'medical_superintendent_firstname', 'medical_superintendent_lastname', 'medical_superintendent_email', 'medical_superintendent_registration_no', 'medical_superintendent_qualification', 'medical_superintendent_mobile', 'pollution_clearance_certificate', 'fire_safety_clearance_certificate', 'certificate_of_incorporation', 'bank_name', 'bank_account_no', 'bank_ifs_code', 'cancel_cheque', 'tariff_list_soc', 'nabh_registration_no', 'nabl_registration_no', 'signed_mous', 'other_documents', 'hrms_software', 'iso_status', 'status', 'comments' ,DB::raw("DATE_FORMAT(hospitals.created_at, '%d-%m-%Y %H:%i:%s')"))->get();
 
         foreach ($admins as $key => $admin) {
             if($admin->assigned_employee){
@@ -23,39 +24,75 @@ class ExportHospital implements FromCollection, WithHeadings
             if($admin->linked_employee){
                 $admins[$key]->linked_employee = Admin::where('id', $admin->linked_employee)->value('firstname').' '.Admin::where('id', $admin->linked_employee)->value('lastname');
             }
-        }
-              
-
+        }              
+                    
         return $admins;
     }
 
     public function headings(): array
     {
         return [
-            'Id',
-            'Uid',
-            'Name',
-            'Onboarding',
-            'By',
-            'Address',
-            'City',
-            'State',
-            'Pincode',
-            'Firstname',
-            'Lastname',
-            'Pan',
-            'Email',
-            'Landline',
-            'Phone',
-            'Rohini',
-            'Linked Associate Partner',
-            'Linked Associate Partner Id',
-            'Assigned Employee',
-            'Assigned Employee Id',
-            'Linked Employee',
-            'Linked Employee Id',
-            'Comments',
-            'Created At',
-        ];
+           'Id',
+           'Uid',
+           'Name',
+           'Onboarding',
+           'By',
+           'Hospital By',
+           'Hms Hospital Id',
+           'Address',
+           'City',
+           'State',
+           'Pincode',
+           'Firstname',
+           'Lastname',
+           'Pan',
+           'Email',
+           'Code',
+           'Landline',
+           'Phone',
+           'Rohini',
+           'Linked Employee Department',
+           'Linked Associate Partner',
+           'Linked Associate Partner Id',
+           'Assigned Employee Department',
+           'Assigned Employee',
+           'Assigned Employee Id',
+           'Linked Employee',
+           'Linked Employee Id',
+           'Tan',
+           'Gst',
+           'Owner Email',
+           'Owner Phone',
+           'Owner Pan',
+           'Owner Aadhar',
+           'Contact Person Firstname',
+           'Contact Person Lastname',
+           'Contact Person Email',
+           'Contact Person Phone',
+           'Registration No',
+           'Medical Superintendent Firstname',
+           'Medical Superintendent Lastname',
+           'Medical Superintendent Email',
+           'Medical Superintendent Registration No',
+           'Medical Superintendent Qualification',
+           'Medical Superintendent Mobile',
+           'Pollution Clearance Certificate',
+           'Fire Safety Clearance Certificate',
+           'Certificate Of Incorporation',
+           'Bank Name',
+           'Bank Account No',
+           'Bank Ifs Code',
+           'Cancel Cheque',
+           'Tariff List Soc',
+           'Nabh Registration No',
+           'Nabl Registration No',
+           'Signed Mous',
+           'Other Documents',
+           'Hrms Software',
+           'Iso Status',
+           'Status',
+           'Comments',
+           'Created At'
+       ];
     }
 }
