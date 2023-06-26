30664566c918a3c8bebb8cf7e84a6dcfddfe6e65
Krishna Mishra <er.krishna.mishra@gmail.com>
2023-06-26 17:21:26 +0530

    chnage

--- app/Exports/ExportHospital.php
+++ app/Exports/ExportHospital.php
@@ -16,7 +16,7 @@ class ExportHospital implements FromCollection, WithHeadings, ShouldAutoSize
     */
     public function collection()
     {
-        $admins = Hospital::latest('id')->select('id','uid', 'name', 'onboarding', 'by', 'hospital_by', 'hms_hospital_id', 'address', 'city', 'state', 'pincode', 'firstname', 'lastname', 'pan', 'email',  'code', 'landline', 'phone', 'rohini', 'linked_employee_department', 'linked_associate_partner', 'linked_associate_partner_id', 'assigned_employee_department', 'assigned_employee', 'assigned_employee_id', 'linked_employee', 'linked_employee_id', 'tan', 'gst', 'owner_email', 'owner_phone', 'owner_pan', 'owner_aadhar', 'contact_person_firstname', 'contact_person_lastname', 'contact_person_email', 'contact_person_phone', 'registration_no', 'medical_superintendent_firstname', 'medical_superintendent_lastname', 'medical_superintendent_email', 'medical_superintendent_registration_no', 'medical_superintendent_qualification', 'medical_superintendent_mobile', 'pollution_clearance_certificate', 'fire_safety_clearance_certificate', 'certificate_of_incorporation', 'bank_name', 'bank_account_no', 'bank_ifs_code', 'cancel_cheque', 'tariff_list_soc', 'nabh_registration_no', 'nabl_registration_no', 'signed_mous', 'other_documents', 'hrms_software', 'iso_status', 'status', 'comments' ,DB::raw("DATE_FORMAT(hospitals.created_at, '%d-%m-%Y %H:%i:%s')"))->get();
+        $admins = Hospital::latest('id')->select('id','uid', 'name', 'onboarding', 'by', 'hospital_by', 'hms_hospital_id', 'address', 'city', 'state', 'pincode', 'firstname', 'lastname', 'pan', 'email',  'code', 'landline', 'phone', 'rohini', 'linked_employee_department', 'linked_associate_partner', 'linked_associate_partner_id', 'assigned_employee_department', 'assigned_employee', 'assigned_employee_id', 'linked_employee', 'linked_employee_id', 'tan', 'gst', 'owner_email', 'owner_phone', 'owner_pan', 'owner_aadhar', 'contact_person_firstname', 'contact_person_lastname', 'contact_person_email', 'contact_person_phone', 'registration_no', 'medical_superintendent_firstname', 'medical_superintendent_lastname', 'medical_superintendent_email', 'medical_superintendent_registration_no', 'medical_superintendent_qualification', 'medical_superintendent_mobile', 'pollution_clearance_certificate', 'fire_safety_clearance_certificate', 'certificate_of_incorporation', 'bank_name', 'bank_account_no', 'bank_ifs_code', 'cancel_cheque', 'tariff_list_soc', 'nabh_registration_no', 'nabl_registration_no', 'signed_mous', 'other_documents', 'hrms_software', 'iso_status', 'comments' ,DB::raw("DATE_FORMAT(hospitals.created_at, '%d-%m-%Y %H:%i:%s')"))->get();
 
         foreach ($admins as $key => $admin) {
             if($admin->assigned_employee){
@@ -95,7 +95,6 @@ class ExportHospital implements FromCollection, WithHeadings, ShouldAutoSize
            'Other Documents',
            'Hrms Software',
            'Iso Status',
-           'Status',
            'Comments',
            'Created At'
        ];
